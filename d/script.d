56a4c7443ac0065b62975f634b1abc465df643d3
drug007 <drug2004@bk.ru>
2020-05-21 11:02:37 +0300

    less braces

--- d/script.d
+++ d/script.d
@@ -8,18 +8,18 @@ import std.datetime.stopwatch: AutoStart, StopWatch;
   ldc2 script.d kernelmatrix.d arrays.d -O5 --boundscheck=off --ffast-math && ./script
 */
 
-auto bench(alias K, T)(K!(T) Kernel, long[] n, bool verbose = true)
+auto bench(alias K, T)(K!T Kernel, long[] n, bool verbose = true)
 {
   auto times = new double[n.length];
   auto sw = StopWatch(AutoStart.no);
   foreach(i; 0..n.length)
   {
     double[3] _times;
-    Matrix!(T) data = createRandomMatrix!(T)(784L, n[i]);
+    auto data = createRandomMatrix!T(784L, n[i]);
     foreach(j; 0..3)
     {
       sw.start();
-      auto mat = calculateKernelMatrix!(K!(T), T)(Kernel, data);
+      auto mat = calculateKernelMatrix!(K!T, T)(Kernel, data);
       sw.stop();
       _times[j] = cast(double)sw.peek.total!"usecs"/1000_000;
       sw.reset();
@@ -34,7 +34,7 @@ auto bench(alias K, T)(K!(T) Kernel, long[] n, bool verbose = true)
 //bench: [0.009412, 0.271136, 1.43589, 11.267, 31.0559]
 void main()
 {
-  auto Kernel = new DotProduct!(float)();
+  auto Kernel = new DotProduct!float();
   writeln("bench: ", bench(Kernel, [1000L, 5000L, 
       10_000L, 20_000L, 30_000L]));
 }
