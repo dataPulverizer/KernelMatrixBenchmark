883d06cdf98f5c61630fd27b5dd7ac52f3fe1c0a
dataPulverizer <data.pulverizer@gmail.com>
2020-05-29 01:21:16 +0100

    Add files via upload

--- images/charts.r
+++ images/charts.r
@@ -1,5 +1,6 @@
 require(ggplot2)
 
+# For language benchmarks
 results = Map(read.csv, c("../data/chapelBench.csv", "../data/dBench.csv", 
                      "../data/juliaBench.csv"))
 results = Reduce(rbind.data.frame, results)
@@ -13,3 +14,16 @@ jpeg(file = "benchplot.jpg", width = 7, height = 5, units = "in", res = 200)
 plot(p)
 dev.off()
 
+# Difference between NDSlice and My basic matrix implementation
+results = Map(read.csv, c("../data/dNDSliceBench.csv", "../data/dBench.csv"))
+results[[1]]$language = "NDSlice"
+results[[2]]$time = results[[2]]$time - results[[1]]$time
+results = results[[2]]
+
+p = ggplot(results, aes(x = nitems, y = time, color = kernel)) + geom_line() + 
+       geom_point() + theme(legend.position="top") + ylab("time (s)") + 
+       xlab("#items")
+
+jpeg(file = "ndsliceDiagnostic.jpg", width = 6, height = 4, units = "in", res = 200)
+plot(p)
+dev.off()
