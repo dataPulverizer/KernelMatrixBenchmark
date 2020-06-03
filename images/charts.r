bc41657f57dc50c052c4c65eea05bd6679e345e3
dataPulverizer <data.pulverizer@gmail.com>
2020-06-04 00:16:28 +0100

    Updated chart and ndsliceDisgnostic.png image file

--- images/charts.r
+++ images/charts.r
@@ -17,13 +17,13 @@ dev.off()
 # Difference between NDSlice and My basic matrix implementation
 results = Map(read.csv, c("../data/dNDSliceBench.csv", "../data/dBench.csv"))
 results[[1]]$language = "NDSlice"
-results[[2]]$time = results[[2]]$time - results[[1]]$time
+results[[2]]$time = 100*(results[[2]]$time - results[[1]]$time)/results[[2]]$time
 results = results[[2]]
 
-p = ggplot(results, aes(x = nitems, y = time, color = kernel)) + geom_line() + 
-       geom_point() + theme(legend.position="top") + ylab("time (s)") + 
-       xlab("#items")
+p = ggplot(results, aes(x = nitems, y = time, fill = kernel)) + geom_col() + 
+       theme(legend.position="none", plot.title = element_text(hjust = 0.5)) + ylab("% Difference\n(+ive = ndslice is faster)") + 
+       xlab("#items") + facet_wrap(~ kernel, scale = "free_y") + ggtitle("Matrix and NDSlice percentage time difference")
 
-jpeg(file = "ndsliceDiagnostic.jpg", width = 6, height = 4, units = "in", res = 200)
+jpeg(file = "ndsliceDiagnostic.jpg", width = 7, height = 7, units = "in", res = 200)
 plot(p)
 dev.off()
