3874d1c4e2da33f2b56063d1d3cd8e0c341fe9c0
dataPulverizer <data.pulverizer@gmail.com>
2020-06-10 23:23:03 +0100

    Add files via upload

--- charts/charts.r
+++ charts/charts.r
@@ -2,8 +2,8 @@ require(data.table)
 require(ggplot2)
 require(scales)
 
-# For language benchmarks
-createPlot = function(folder)
+# Plots for language benchmarks
+createJPGPlot = function(folder, filename)
 {
   results = Map(fread, c(paste0("../", folder,"/chapelBench.csv"), 
                          paste0("../", folder,"/dBench.csv"), 
@@ -15,33 +15,74 @@ createPlot = function(folder)
                   labels = trans_format("log10", math_format(10^.x))) + 
          theme(legend.position="top") + ylab("time (s)") + 
          xlab("Number Of Items") + facet_wrap(~ kernel, scale = "free_y")
-  return(p)
+  jpeg(file = filename, width = 9, height = 7, units = "in", res = 200)
+  plot(p)
+  dev.off()
+  return(invisible(p))
+}
+createSVGPlot = function(folder, filename)
+{
+  results = Map(fread, c(paste0("../", folder,"/chapelBench.csv"), 
+                         paste0("../", folder,"/dBench.csv"), 
+                     paste0("../", folder,"/juliaBench.csv")))
+  results = rbindlist(results)
+  results[, kernel := gsub(" ", "", kernel)]
+  p = ggplot(results, aes(x = nitems, y = time, color = language)) + geom_line() + 
+         geom_point() + scale_y_continuous(trans = "log10",
+                  labels = trans_format("log10", math_format(10^.x))) + 
+         theme(legend.position="top") + ylab("time (s)") + 
+         xlab("Number Of Items") + facet_wrap(~ kernel, scale = "free_y")
+  svg(file = filename, width = 9, height = 7)
+  plot(p)
+  dev.off()
+  return(invisible(p))
 }
 
-p1 = createPlot("data")
-p2 = createPlot("fmdata")
+createSVGPlot("data", "benchplot.svg")
+createSVGPlot("fmdata", "fmbenchplot.svg")
 
-jpeg(file = "benchplot.jpg", width = 9, height = 7, units = "in", res = 200)
-plot(p1)
-dev.off()
-
-jpeg(file = "fmbenchplot.jpg", width = 9, height = 7, units = "in", res = 200)
-plot(p2)
-dev.off()
+createJPGPlot("data", "benchplot.jpg")
+createJPGPlot("fmdata", "fmbenchplot.jpg")
 
 
 # Difference between NDSlice and My basic matrix implementation
-results = Map(fread, c("../data/dNDSliceBench.csv", "../data/dBench.csv"))
-results[[1]][, language := "NDSlice"]
-results[[2]][, time := 100*(time - results[[1]][,time])/time]
-results = results[[2]]
-
-p3 = ggplot(results, aes(x = nitems, y = time, fill = kernel)) + geom_col() + 
+createJPGNDSlicePlot = function(filename)
+{
+  results = Map(fread, c("../data/dNDSliceBench.csv", "../data/dBench.csv"))
+  results[[1]][, language := "NDSlice"]
+  results[[2]][, time := 100*(time - results[[1]][,time])/time]
+  results = results[[2]]
+  
+  p = ggplot(results, aes(x = nitems, y = time, fill = kernel)) + geom_col() + 
        theme(legend.position="none", plot.title = element_text(hjust = 0.5)) + 
        ylab("% Difference\n(+ive = ndslice is faster)") + 
        xlab("Number Of Items") + facet_wrap(~ kernel, scale = "free_y") + 
        ggtitle("Matrix and NDSlice percentage time difference")
+  
+  jpeg(file = filename, width = 7, height = 7, units = "in", res = 200)
+  plot(p)
+  dev.off()
+  return(invisible(p))
+}
+# "ndsliceDiagnostic.jpg"
+createSVGNDSlicePlot = function(filename)
+{
+  results = Map(fread, c("../data/dNDSliceBench.csv", "../data/dBench.csv"))
+  results[[1]][, language := "NDSlice"]
+  results[[2]][, time := 100*(time - results[[1]][,time])/time]
+  results = results[[2]]
+  
+  p = ggplot(results, aes(x = nitems, y = time, fill = kernel)) + geom_col() + 
+       theme(legend.position="none", plot.title = element_text(hjust = 0.5)) + 
+       ylab("% Difference\n(+ive = ndslice is faster)") + 
+       xlab("Number Of Items") + facet_wrap(~ kernel, scale = "free_y") + 
+       ggtitle("Matrix and NDSlice percentage time difference")
+  
+  svg(file = filename, width = 7, height = 7)
+  plot(p)
+  dev.off()
+  return(invisible(p))
+}
 
-jpeg(file = "ndsliceDiagnostic.jpg", width = 7, height = 7, units = "in", res = 200)
-plot(p3)
-dev.off()
+createJPGNDSlicePlot("ndsliceDiagnostic.jpg")
+createSVGNDSlicePlot("ndsliceDiagnostic.svg")
